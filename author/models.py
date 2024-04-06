8db8e93299028b9d86caf68dbb6677c79d3695ed
Андрей Тетерин <124205878+StarAres1@users.noreply.github.com>
2024-04-06 08:11:46 +0000

    Fix JavaScript

--- author/models.py
+++ author/models.py
@@ -5,3 +5,4 @@ from django.contrib.auth.models import Group, Permission
 class User(AbstractUser):
   groups = models.ManyToManyField(Group, related_name='myuser_groups')
   user_permissions = models.ManyToManyField(Permission, related_name='myuser_user_permissions')
+  models.ForeignKey(AbstractUser, on_delete=models.CASCADE)
