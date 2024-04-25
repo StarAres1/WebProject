3e00a126b7400a34651d8ac66b7744a852e14f34
Андрей Тетерин <124205878+StarAres1@users.noreply.github.com>
2024-04-25 20:06:22 +0000

    dimam comments 50%
    Каждые n секунд на сайт присылается объект json с комментариями, которых еще нет на странице

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('description/<int:object_id>/', views.description, name='description'),
     path('new/', views.new, name='new'),
-    path('add_comment/', views.add_comment, name='add_comment')
+    path('add_comment/', views.add_comment, name='add_comment'),
+    path('get_new_comments/', views.GetNewComments.as_view(), name='get_new_comments')
     
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, redirect
-from . models import Cards, Comment
+from .models import Cards, Comment
 from .forms import CommentForm
 from django.http import JsonResponse
+
+import json
+from django.http import HttpResponse
+from django.views import View
+from django.template.loader import render_to_string
+
+from datetime import datetime
+from dateparser import parse
+from django.core import serializers
+
 
 def index(request):
   cards = Cards.objects.all()
   return render(request, 'main/index.html', {'cards': cards})
+
 
 def description(request, object_id):
   error = ''
@@ -18,17 +29,18 @@ def description(request, object_id):
       return redirect(line)
     else:
       error = form.errors
-      
+
   form = CommentForm()
-  
+
   object = Cards.objects.get(id=object_id)
-  сomments = Comment.objects.filter(cat=object)
+  сomments = Comment.objects.filter(cat=object).order_by('-time')
   return render(request, 'main/page gow.html', {
-    'object': object, 
-    'comments': сomments, 
-    'form': form,
-    'error': error
+      'object': object,
+      'comments': сomments,
+      'form': form,
+      'error': error
   })
+
 
 def new(request):
   return render(request, 'main/new.html')
@@ -36,20 +48,40 @@ def new(request):
 
 def add_comment(request):
   form = CommentForm()
-  if request.method == "POST" and request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-      form = CommentForm(request.POST)
-      if form.is_valid():
-          text = form.cleaned_data['text']
-          author = form.cleaned_data['author']            
-        
-          form.save()
-          return JsonResponse({
-            "text": text,
-            "author": author,
-            }, status=200)
-      else:
-          errors = form.errors
-          print(errors)
-          return JsonResponse(errors, status=400)   
-  
+  if request.method == "POST" and request.headers.get(
+      'X-Requested-With') == 'XMLHttpRequest':
+    form = CommentForm(request.POST)
+    if form.is_valid():
+      text = form.cleaned_data['text']
+      author = form.cleaned_data['author']
+
+      form.save()
+      return JsonResponse({
+          "text": text,
+          "author": author,
+      }, status=200)
+    else:
+      errors = form.errors
+      print(errors)
+      return JsonResponse(errors, status=400)
+
   return render(request, "../", {"form": form})
+
+
+class GetNewComments(View):
+
+
+  
+  def post(self, request):
+    сomments = Comment.objects.filter(cat=request.POST.get('id_cat')).order_by('-time')
+
+    result = request.POST.get('time').strip()   
+
+    mas = Comment.objects.filter(time__gt=parse(result, languages=['ru'])).exclude(time=parse(result, languages=['ru']))
+    print(mas)
+    if len(mas) == 0:
+      return JsonResponse({'mesage': "0"})
+    else:
+      mas_json = serializers.serialize('json', mas[1:])
+      return JsonResponse({'mesage': "1", "mas": mas_json})
+
